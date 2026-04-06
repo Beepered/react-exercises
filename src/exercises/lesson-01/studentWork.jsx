a6472d69b1e1b968b0532f5643a2573f38c25699
Beepered <bktrieu@gmail.com>
2026-04-06 16:19:42 -0700

    complete lesson

--- src/exercises/lesson-01/studentWork.jsx
+++ src/exercises/lesson-01/studentWork.jsx
@@ -3,10 +3,26 @@
 
 export default function StudentWork() {
   //add variables here
+  let name = "Brendan Trieu";
+  let age = 99;
+  let hobbies = [
+    "eating",
+    "sleeping",
+    "lying"
+  ]
   return (
     <div>
       {/* add JSX here */}
       <p> Student output will go here </p>
+      <h1>About {name}</h1>
+      <h2>Descripion:</h2>
+      <p>Blah blah blah. Some paragraph.</p>
+
+      <h2>My hobbies</h2>
+      <ul>
+        {hobbies.map(hobby => <li>{hobby}</li>)}
+      </ul>
+      
     </div>
   );
 }
